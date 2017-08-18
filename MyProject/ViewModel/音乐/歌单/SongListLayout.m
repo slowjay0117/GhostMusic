c40d667942e9d60462c2b93b06665b8925eaa0a7
slowjay0117 <will@willdeMacBook-Pro.local>
2017-08-18 15:25:14 +0800

    创建歌单列表的布局
    创建歌单列表的布局

--- MyProject/ViewModel/音乐/歌单/SongListLayout.m
+++ MyProject/ViewModel/音乐/歌单/SongListLayout.m
@@ -10,4 +10,20 @@
 
 @implementation SongListLayout
 
+- (instancetype)init{
+    if (self = [super init]) {
+        self.minimumLineSpacing = 10;
+        self.minimumInteritemSpacing = 10;
+        self.sectionInset = UIEdgeInsetsMake(10, 20, 10, 20);
+        //计算一行显示三个时的宽度
+        CGFloat width = (long)((kWindowW-2*20-2*10)/3);
+        CGFloat height = width + 75;
+        self.itemSize = CGSizeMake(width, height);
+        self.scrollDirection = UICollectionViewScrollDirectionVertical;
+        //设置分区头大小
+        self.headerReferenceSize = CGSizeMake(0, 45);
+    }
+    return self;
+}
+
 @end
