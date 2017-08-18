805330aac5384adaa7cdc718c403b3631c8d8e40
slowjay0117 <will@willdeMacBook-Pro.local>
2017-08-18 16:48:30 +0800

    歌单的进阶显示
    添加了图片右下角的文本，使用NSTextAttachment和NSAttributedString来实现

--- MyProject/ViewController/音乐/歌单/SongListViewController.m
+++ MyProject/ViewController/音乐/歌单/SongListViewController.m
@@ -52,7 +52,22 @@
     cell.posterImageView.image = [UIImage imageNamed:fileName];
     cell.titleLabel.text = @"现在就是我想你的时候";
     cell.crearorLabel.text = @"By 限量版不着调的........";
-    cell.listenNumLabel.text = @"2345";
+    
+    //1.创建一个附件，附件内容就是图片
+    NSTextAttachment *audio = [NSTextAttachment new];
+    audio.image = [UIImage imageNamed:@"ic_recommend_audition"];
+    audio.bounds = CGRectMake(0, 0, 10, 10);
+    //2.将附件转换为NSAttributedString类型
+    NSAttributedString *audioString = [NSAttributedString attributedStringWithAttachment:audio];
+    //3.将附件插入到指定的带有属性的字符串的某个位置上
+    NSMutableAttributedString *string = [[NSMutableAttributedString alloc]initWithString:@" 4872"];
+    [string insertAttributedString:audioString atIndex:0];
+    //4.为带有属性的字符串添加属性设置
+    [string addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor] range:NSMakeRange(0, string.length)];
+    //[string addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(3, 1)];
+    //[string addAttribute:NSKernAttributeName value:@8 range:NSMakeRange(0, string.length)];
+    //5.将带有属性的字符串赋给负责显示文本的标签、按钮等控件
+    cell.listenNumLabel.attributedText = string;
     return cell;
 }
 @end
